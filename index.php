a7330312f4c0720090b44da68905790231282455
esaniello <shaniel.samadhan.natin@gmail.com>
2018-10-29 22:22:17 -0300

    Adding games

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Think Tank</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <h1>welcome to ThinkTank!</h1>
+
+    <iframe width='640' height='480' frameborder='0' scrolling='no' src='http://www.4j.com/embed/Mini-Scientist'></iframe>
+</body>
+</html>
