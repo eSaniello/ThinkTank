ef2bd5fa25b9a3cfa2d2219b3d323259cfb03163
esaniello <shaniel.samadhan.natin@gmail.com>
2018-12-06 13:32:23 -0300

    Updated the look of the login and register page

--- backend/register.php
+++ backend/register.php
@@ -10,29 +10,38 @@
 
        if(empty($firstname) || empty($lastname) || empty($name) || empty($pw) || empty($pw01))
        {
-           header("Location: ../front-end/register.php?register=empty&firstname=$firstname&lastname=$lastname&user_name=$name");
+           header("Location: ../index.php?register=empty");
            exit();
        }
        else
        {
            if($pw != $pw01)
            {
-                header("Location: ../front-end/register.php?register=verify&firstname=$firstname&lastname=$lastname&user_name=$name");
+                header("Location: ../index.php?register=verify");
                 exit();
            }
            elseif($pw == $pw01)
            {
+               $sql = "SELECT * from login where user_name = '$name'";
+               $query = mysqli_query($connection, $sql); 
+               $row = mysqli_fetch_array($query);
+               if($row['user_name'] == null){
                 $pw_hash = password_hash($pw , PASSWORD_DEFAULT);
-                $sql_query = "insert into login(firstname, lastname, user_name, password) values('$firstname', '$lastname', '$name' , '$pw_hash');";
+                $sql_query = "INSERT into login(firstname, lastname, user_name, password) values('$firstname', '$lastname', '$name' , '$pw_hash');";
                 $result = mysqli_query($connection , $sql_query);
                 header("Location: ../index.php?registration=succes");
                 exit();
+               }
+               else{
+                   header("Location: ../index.php?register=exists");
+                   exit();
+               }
            }
        }
 
    }  
    else
    {
-       header("location: ../front-end/register.php?error");
+       header("location: ../index.php?error");
        exit();
    }
--- index.php
+++ index.php
@@ -1,63 +1,124 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" >
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>||ThinkTank|| Log In/Register</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
-</head>
-<body class= "index_body">
+  <meta charset="UTF-8">
+  <title>||ThinkTank|| Login & Signup</title>
   
-    <div align="center">
+  
+  
+      <link rel="stylesheet" href="css/style.css">
 
-    <form method="POST" action="backend/login.php" class="form">
-        <h1>Log in</h1>
-        <br>    
-        <?php
-        if(isset($_GET["user_name"]))
-        {
-            $user = $_GET["user_name"];
-            echo '<input type="text" placeholder="User name" name="user_name" value="'.$user.'" class="gen_input"><br>';
-        }
-        else
-        {
-            echo '<input type="text" placeholder="User name" name="user_name" class="gen_input"><br>';
-        }
-        ?>
-        <br>
-        <input type="password" placeholder="Password" name="password" class="gen_input"><br><br>
-        <button type="submit" name="login" class='login'>Log in</button><br><br>
-        <button type="submit" name="register" class='new'>New user?</button><br><br>
-    </form>
-    <?php
+  
+</head>
 
-        if(!isset($_GET["login"]))
-        {
-          exit();
-        }
-        else
-        {
-             $formCheck = $_GET["login"];
+<body>
+  
+  <h1 id="titel">ThinkTank</h1>
 
-            if($formCheck == "empty")
-            {
-                //javascript pop up
-                echo "<script> alert('Form is empty! Please fill in all fields!'); </script>";
-                exit();
-            }
-            elseif($formCheck == "failed"){
-                //javascript pop up
-                echo "<script> alert('Login failed. Please type in correct username and password.'); </script>";
-                exit();
-            }
-            elseif($formCheck == "succes")
-            {
-                header("Location: game.php?login=succes");
-                exit();
-            }
-        }
-    ?>
+  <div class="container">
+  <div class="box"></div>
+  <div class="container-forms">
+    <div class="container-info">
+      <div class="info-item">
+        <div class="table">
+          <div class="table-cell">
+            <p>
+              Have an account?
+            </p>
+            <div class="btn">
+              Log in
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="info-item">
+        <div class="table">
+          <div class="table-cell">
+            <p>
+              Don't have an account? 
+            </p>
+            <div class="btn">
+              Sign up
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-form">
+      <div class="form-item log-in">
+        <div class="table">
+          <div class="table-cell">
+            <form method="POST" action="backend/login.php">
+              <input name="user_name" placeholder="Username" type="text" />
+              <input name="password" placeholder="Password" type="Password" />
+              <input type='submit' name="login" value="Log in" class="btn" />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="form-item sign-up">
+        <div class="table">
+          <div class="table-cell">
+            <form method="POST" action="backend/register.php">
+              <input name="firstname" placeholder="Firstname" type="text" />
+              <input name="lastname" placeholder="Lastname" type="text" />
+              <input name="user_name" placeholder="Username" type="text" />
+              <input name="password" placeholder="Password" type="Password" />
+              <input name="password01" placeholder="Verify Password" type="Password" />
+              <input type="submit" name="register" value="Register" class="btn">
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+  <script  src="js/index.js"></script>
+
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+if(!isset($_GET["login"]) && !isset($_GET["register"]))
+{
+  exit();
+}
+else
+{
+     $formCheck = $_GET["login"];
+     $formCheck1 = $_GET["register"];
+
+    if($formCheck == "empty" || $formCheck1 == "empty")
+    {
+        //javascript pop up
+        echo "<script> alert('Form is empty! Please fill in all fields!'); </script>";
+        exit();
+    }
+    elseif($formCheck == "failed"){
+        //javascript pop up
+        echo "<script> alert('Login failed. Please type in correct username and password.'); </script>";
+        exit();
+    }
+    elseif($formCheck1 == "verify")
+    {
+        //javascript pop up
+        echo "<script> alert('Passwords must be the same!'); </script>";
+        exit();
+    }
+    elseif($formCheck1 == "exists"){
+       //javascript pop up
+       echo "<script> alert('User already exists!'); </script>";
+       exit();
+    }
+    elseif($formCheck == "succes")
+    {
+        header("Location: game.php?login=succes");
+        exit();
+    }
+}
+?>
+
+
 </body>
+
 </html>
